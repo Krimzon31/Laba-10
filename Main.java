f6315bd71d2652e7a4ea678f75d442bc527140a2
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2022-04-15 11:10:21 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,10 +10,10 @@ public class Main {
         System.out.println("Введите строку содержащую верный IP-адрес");
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        Pattern P = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.(?!$)|$)){4}");
+        Pattern P = Pattern.compile("\\b([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\b");
         Matcher m = P.matcher(str);
         if (m.find()){
-            System.out.println("Корректный IP-адрес:" + m.group());
+            System.out.println("Корректный IP-адрес: " + m.group());
         }
         else {
             System.out.println("IP-адрес в строке не коректен");
